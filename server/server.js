0e73ad3d723ecda195369b528943e2cb6c7d6769
Vastille Dolcine <vdolcine@gmail.com>
2022-03-01 17:24:39 -0800

    Preparing for a PR

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const consumerRouter = require('./routes/consumerRouter');
-const partitionRouter = require('./routes/partitionRouter');
-const producerRouter = require('./routes/producerRouter');
-const topicRouter = require('./routes/topicsRouter');
-const promPortRouter = require('./routes/promPortRouter');
+const consumerRouter = require("./routes/consumerRouter");
+const partitionRouter = require("./routes/partitionRouter");
+const producerRouter = require("./routes/producerRouter");
+const topicRouter = require("./routes/topicsRouter");
+const promPortRouter = require("./routes/promPortRouter");
 
 app.use(cors());
 app.use(express.json());
@@ -22,20 +22,19 @@ app.use(express.urlencoded({ extended: true }));
 //     res.status(200).sendFile(path.join(__dirname, '../public/index.html'))
 //   );
 // }
-app.use('/api/prom-port', promPortRouter)
-app.use('/api/consumer', consumerRouter);
-app.use('/api/partition', partitionRouter);
-app.use('/api/producer', producerRouter);
-app.use('/api/topic', topicRouter);
-
+app.use("/api/prom-port", promPortRouter);
+app.use("/api/consumer", consumerRouter);
+app.use("/api/partition", partitionRouter);
+app.use("/api/producer", producerRouter);
+app.use("/api/topic", topicRouter);
 
 app.use((req, res) => res.sendStatus(404));
 
 app.use((err, req, res, next) => {
   const defaultErr = {
-    log: 'Express error handler caught unknown middleware error',
+    log: "Express error handler caught unknown middleware error",
     status: 400,
-    message: { err: 'An error occurred ' },
+    message: { err: "An error occurred " },
   };
   const errorObj = Object.assign(defaultErr, err);
   console.log(errorObj);
