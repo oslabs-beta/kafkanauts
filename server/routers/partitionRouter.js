e251e2f87572af49ad479bb16f9e10d0986d8f10
david lee <david.lee7731@yahoo.com>
2022-03-03 21:08:52 -0500

    hi

--- server/routers/partitionRouter.js
+++ server/routers/partitionRouter.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const partitionController = require('../controllers/partitionController.js');
+const { getSavedPortFromElectronStore } = require('../controllers/promPortController');
 
-router.get('/partition', partitionController.totalPartitionCount, (req, res) => {
+
+router.get('/partition', getSavedPortFromElectronStore, partitionController.totalPartitionCount, (req, res) => {
   return res.status(200).json(res.locals /*.whatever*/);
 });
 
-router.get('/partition', partitionController.offlinePartitionCount, (req, res) => {
+router.get('/partition', getSavedPortFromElectronStore, partitionController.offlinePartitionCount, (req, res) => {
   return res.status(200).json(res.locals /*.whatever*/);
 });
 
