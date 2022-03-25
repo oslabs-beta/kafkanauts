da869c48116062d61dac291c28daee44b2e81239
Jane Park <park.jane.sun@gmail.com>
2022-03-24 19:42:40 -0700

    save before compare to dev

--- server/controllers/promPortController.ts
+++ server/controllers/promPortController.ts
@@ -1,16 +1,18 @@
-const axios = require('axios');
-const ElectronStore = require('electron-store');
+import { Request, Response, NextFunction } from 'express';
+import axios from 'axios';
+import ElectronStore from 'electron-store';
+
 const schema = {
   port: {
-    type: 'string',
+      type: 'string',
   },
   shellName: {
-    type: 'string',
+      type: 'string',
   }
-}
-const db = new ElectronStore({schema});
+};
+const db = new ElectronStore({ schema });
 const promPortController = {
-  async isPromPortUp(req, res, next) {
+  async isPromPortUp(req: Request, res: Response, next: NextFunction) {
     const { port } = req.body;
     // https://stackoverflow.com/questions/12968093/regex-to-validate-port-number#comment89586361_12968117
     const convertStringToNum = Number(port);
@@ -27,14 +29,14 @@ const promPortController = {
       return next(e);
     }
   },
-  savePortToElectronStore(req, res, next) {
+  savePortToElectronStore(req: Request, res: Response, next: NextFunction) {
     const { port, shellName } = req.body;
     db.set('port', port);
     db.set('shellName', shellName);
     return next();
   },
   
-  getSavedPortFromElectronStore(req, res, next) {
+  getSavedPortFromElectronStore(req: Request, res: Response, next: NextFunction) {
     const port = db.get('port');
     res.locals.port = port;
     return next();
