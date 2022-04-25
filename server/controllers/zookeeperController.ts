4944ebabb26e2a9fbcd3b787da66b9f61b5c1a54
Jane Park <park.jane.sun@gmail.com>
2022-04-25 08:57:18 -0700

    added zookeeper and controller metrics

--- server/controllers/zookeeperController.ts
+++ server/controllers/zookeeperController.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import axios from 'axios';
 
 const zookeeperController = {
+  //this data does not change so it's not best for graphs
   async avg_latency (req: Request, res: Response, next: NextFunction) {
     try {
       const { port } = res.locals;
