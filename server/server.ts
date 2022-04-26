f18552d71183a96a1c1f1031fbac66d5878a3de2
David Lee <david.lee7731@yahoo.com>
2022-04-26 09:19:28 -0400

    initial commit

--- server/server.ts
+++ server/server.ts
@@ -1,26 +1,16 @@
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import partitionRouter from './routers/partitionRouter';
 import producerRouter from './routers/producerRouter';
 import topicRouter from './routers/topicsRouter';
 import promPortRouter from './routers/promPortRouter';
 import consumerRouter from './routers/consumerRouter';
 import zookeeperRouter from './routers/zookeeperRouter';
-// import { partitionController } from './controllers/partitionController';
-// import { producerController } from './controllers/producerController';
-// import { promPortController } from './controllers/promPortController';
-// import { topicsController } from './controllers/topicsController';
-// import { consumerController } from './controllers/consumerController';
-import express, { Request, Response, NextFunction } from 'express';
 import { ServerError } from '../types';
-
-
-const path = require('path');
-const cors = require('cors');
-require('dotenv').config();
+import 'dotenv/config';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-
-// const consumerRouter = require('./routers/consumerRouter');
 
 app.use(cors());
 app.use(express.json());
@@ -39,22 +29,11 @@ app.use('/api/producer', producerRouter);
 app.use('/api/topic', topicRouter);
 app.use('/api/zookeeper', zookeeperRouter);
 
-
 app.use((req, res) => res.sendStatus(404));
 
 //global error handler
 app.use((err: ServerError, req: Request, res: Response, next: NextFunction) => {
-  const defaultError = {
-    log: 'Express error handler caught unknown middleware error',
-    status: 400,
-    message: { err: 'An error occurred'},
-  };
-
-  const errorObj = Object.assign({}, defaultError, err);
-  console.log(errorObj.log);
-  return res.status(errorObj.status).json(errorObj.message);
+  return res.status(err.status ?? 500).json(err.message ?? 'Internal Server Error');
 })
 
-app.listen(PORT, () => {
-  console.log(`Listening on PORT ${PORT}...`);
-});
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}...`));
